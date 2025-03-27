26a22368cff98000cc2bfa0b6247261809621dbb
Fellas <neuneu12@naver.com>
2025-03-27 14:25:46 +0900

    .

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from store.models import Category, Product
 
 # Register your models here.
+# dev_3
+admin.site.register(Category)
+admin.site.register(Product)
--- store/models.py
+++ store/models.py
@@ -1,3 +1,21 @@
+from unicodedata import category
 from django.db import models
 
 # Create your models here.
+# dev_3
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __srt__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(default=0, max_digits=6, decimal_places=2) # 9999.99
+    description = models.CharField(max_length=250, default="", blank=True, null=True)
+    image = models.ImageField(upload_to="upload/product")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __srt__(self):
+        return self.name
