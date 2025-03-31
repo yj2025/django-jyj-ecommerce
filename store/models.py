e3339911578dfefd8e2136640bc37b1c68a66735
Fellas <neuneu12@naver.com>
2025-03-31 10:48:19 +0900

    .

--- store/models.py
+++ store/models.py
@@ -1,3 +1,4 @@
+from pyexpat import model
 from django.db import models
 
 
@@ -16,6 +17,10 @@ class Product(models.Model):
     description = models.CharField(max_length=250, default="", blank=True, null=True)
     image = models.ImageField(upload_to="upload/product")
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    
+    # dev_6
+    is_sale = models.BooleanField(default=False)
+    sale_price = models.IntegerField(default=0)
 
     def __str__(self):
         return self.name
