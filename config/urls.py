50daae7ad21123421d54b3ce7e5cbc82acd07047
Fellas <neuneu12@naver.com>
2025-03-31 20:20:26 +0900

    .

--- config/urls.py
+++ config/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include('store.urls')), # dev_1
+    path("", include("store.urls")),  # dev_1
 ]
 
 # dev_2
@@ -15,5 +15,4 @@ urlpatterns = [
 
 # dev_2
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
-
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
