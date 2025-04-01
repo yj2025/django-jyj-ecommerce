2730b96214e6221e004fbe161b85f58967cf8cce
Fellas <neuneu12@naver.com>
2025-04-01 14:26:06 +0900

    .

--- accounts/views.py
+++ accounts/views.py
@@ -3,19 +3,21 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 # from .forms import RegisterUserForm # 상대 경로형식
-from accounts.forms import RegisterUserForm # 절대 경로 형식
+from accounts.forms import RegisterUserForm  # 절대 경로 형식
 
 # Create your views here.
+
 
 # class HTTPRequest:
 #    POST = {"username":"admim","password":"1234"}
 #    GET = {}
-
 # dev_9
 
+
 def logout_user(request):
-    logout(request) # session에 저장된 sessionid 삭제
+    logout(request)  # session 에 저장된 sessionid 삭제
     return redirect("/")
+
 
 def login_user(request):
 
@@ -34,6 +36,7 @@ def login_user(request):
     else:
         return render(request, "accounts/login.html", {})
 
+
 # dev_10
 def register_user(request):
 
@@ -41,18 +44,7 @@ def register_user(request):
 
     if request.method == "POST":
         print(form)
-        # username = request.POST["username"]
-        # password = request.POST["password"]
-        # user = authenticate(request, username=username, password=password)
-
-        # if user is not None:
-        #     login(request, user)
-        #     messages.success(request, "You Have been logged in")
-        #     return redirect("/")
-        # else:
-        #     messages.success(request, ("There was an error, please try again"))
-        #     return redirect("accounts:login_user")
     else:
-        context = {"form":form}
+        context = {"form": form}
 
     return render(request, "accounts/register.html", context)
