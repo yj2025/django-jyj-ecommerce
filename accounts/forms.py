b1ab1c9ca7f93c8f4264ca1f9c45d355af081eb5
Fellas <neuneu12@naver.com>
2025-04-01 14:23:57 +0900

    .

--- accounts/forms.py
+++ accounts/forms.py
@@ -12,3 +12,19 @@ class RegisterUserForm(UserCreationForm):
             "job",
             "gender",
         ]
+
+#  dev_11
+    def init(self, args, **kwargs):
+        super().init(args, **kwargs)
+
+        # 모든 필드에 Bootstrap form-control 클래스 추가
+        for field_name, field in self.fields.items():
+            field.widget.attrs.update(
+                {"class": "form-control", "placeholder": field.label}
+            )
+
+        # 성별 선택을 위한 Bootstrap form-select 클래스 추가
+        self.fields["gender"].widget.attrs.update({"class": "form-select"})
+
+        # 직업 선택을 위한 Bootstrap form-select 클래스 추가
+        self.fields["job"].widget.attrs.update({"class": "form-select"})
