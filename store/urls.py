dfa650bb6cfb828b33cf00e52f61a37d50306449
Fellas <neuneu12@naver.com>
2025-04-03 09:26:02 +0900

    .

--- store/urls.py
+++ store/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
 from . import views
 
 # dev_8
@@ -8,8 +8,12 @@ app_name = "store"
 # dev_1
 urlpatterns = [
     path("", views.home, name="home"),
-    path("about", views.about, name="about"),  # dev_8 about page 추가
+    path("about", views.about, name="about"),  # dev_8 어바웃 페이지 추가
     path("product/<int:product_id>", views.product, name="product"),  # dev_13 제품상세
-    path("category_summary", views.category_summary, name="category_summary"),  # dev_14 카테고리 처리
-    path("category/<int:category_id>", views.category, name="category"),  # dev_14 카테고리 처리
+    path(
+        "category_summary", views.category_summary, name="category_summary"
+    ),  # dev_14 카테고리 처리
+    path(
+        "category/<int:category_id>", views.category, name="category"
+    ),  # dev_14 카테고리 처리
 ]
