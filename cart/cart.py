5aabf1a6af7609cd03484e0d5cc4b52219c3fa74
Fellas <neuneu12@naver.com>
2025-04-04 13:33:45 +0900

    .

--- cart/cart.py
+++ cart/cart.py
@@ -27,26 +27,25 @@ class Cart:  # 카트 클래스 생성
     def __len__(self):
         return sum(item["quantity"] for item in self.cart.values())
 
-
+    # dev_18
     def __iter__(self):
-        product_ids = self.cart.keys() # ("1":, "2")
+        product_ids = self.cart.keys()  # ("1","2")
 
-
-        # select * from product where id in ("1", "2")
+        # select * from product where id in ("1","2")
         products = Product.objects.filter(id__in=product_ids)
 
         # self.cart = {
-        #       "1",{"quantity": 1, "price": "10000.00", "prodcut": <Product: 상품1> },
-        #       "2",{"quantity": 1, "price": "5000.00", "prodcut": <Product: 상품2> },
-        #       }
+        #          "1":{"quantity":7,"price":3000.00,"product": <Product: 상품1>, "total_price":21000 }
+        #          "2":{"quantity":1,"price":5000.00,"product": <Product: 상품2> , "total_price":50000 }
+        #        }
         for product in products:
             self.cart[str(product.id)]["product"] = product
 
         for item in self.cart.values():
             item["price"] = Decimal(item["price"])
             item["total_price"] = item["price"] * item["quantity"]
-            
-            yield item # 제네레이터 문법
+
+            yield  item  # 제너레이터 문법 return
 
     def add(self, product, quantity=1, is_update=False):
         product_id = str(product.id)
@@ -55,6 +54,7 @@ class Cart:  # 카트 클래스 생성
         #          "1":{"quantity":7,"price":"3000.00"}
         #          "2":{"quantity":1,"price":"5000.00"}
         #        }
+
         if product_id not in self.cart:
             self.cart[product_id] = {"quantity": 0, "price": str(product.price)}
 
@@ -64,11 +64,12 @@ class Cart:  # 카트 클래스 생성
             self.cart[product_id]["quantity"] += quantity
 
         self.save()
-#     self.sesstion =request.sesssion = { 'cart':' {}(self.cart)  }
-#     self.cart = {
-#                       "1",{"quantity": 1, "price": "10000.00"}
-#                      "2":{"quantity":1,"price":"5000.00"}
-#                  }
+
+    #     self.sesstion =request.sesssion = { 'cart':' {}(self.cart)  }
+    #     self.cart = {
+    #                       "1",{"quantity": 1, "price": "10000.00"}
+    #                      "2":{"quantity":1,"price":"5000.00"}
+    #                  }
     def save(self):
         self.session[settings.CART_SESSION_ID] = self.cart
         self.session.modified = True  # 해당 세션을 DB에 저장
