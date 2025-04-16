00ccef76944a09a58236517dbf16fe4a66a0a324
Fellas <neuneu12@naver.com>
2025-04-16 10:30:10 +0900

    .

--- api/serializers.py
+++ api/serializers.py
@@ -2,11 +2,11 @@ from rest_framework import serializers
 from store.models import Category, Product
 
 # 2. Serilaizer 객체의 주요 기능
-# serialization
-# deserialiaztion
-# validation
-# request / response 데이터 핸들링 ( to_internal_value() / to_representation() )
-# nested serialization
+# 1) serialization
+# 2) deserialiaztion
+# 3) validation
+# 4) request / response 데이터 핸들링 ( to_internal_value() / to_representation() )
+# 5) nested serialization
 
 
 # dev_29
@@ -22,6 +22,7 @@ from store.models import Category, Product
 #     is_sale = serializers.BooleanField()
 #     sale_price = serializers.IntegerField()
 
+
 # 객체를 => 딕셔너리로 만드는게 목적
 #
 class ProductSerializer(serializers.ModelSerializer):
@@ -29,3 +30,27 @@ class ProductSerializer(serializers.ModelSerializer):
         model = Product
         fields = "__all__"
         # fields = ["id", "name", "category"]
+
+    # dev_31
+    # 가격은 0 이상 10,000 이하
+    def validate_price(self, value):
+
+        if value < 0:
+            raise serializers.ValidationError("가격은 0 이상이어야 합니다.")
+
+        if value > 100000:
+            raise serializers.ValidationError("가격은 10만원 이하여야 합니다.")
+
+        return value
+
+    # 이름은 3자 이상 100자 이하
+    def validate_name(self, value):
+        if len(value.strip()) < 3:  # 문자열 양끝 공백 제거
+            raise serializers.ValidationError("상품 이름은 최소 3자 이상이어야 합니다.")
+
+        if len(value.strip()) > 100:
+            raise serializers.ValidationError("상품 이름은 100자를 초과 할수 없습니다.")
+
+        return value
+    
+    
